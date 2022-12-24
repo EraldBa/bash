7fa27396f04b0da67411a0555a8269da7b9b1eca
EraldBa <owlyice33@hotmail.com>
2022-12-24 22:38:05 +0200

    Added for deb

---
+++ newdeb.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# For debian systems
+
+sudo apt update && sudo apt upgrade
+wait
--- newfedora.sh
+++ newfedora.sh
@@ -8,7 +8,7 @@ wait
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 wait
 
-sudo dnf install kitty fish neovim bat vlc bpytop firewalld steam
+sudo dnf install kitty fish neovim bat vlc bpytop firewalld steam neofetch clocate
 wait
 
 sudo dnf install dnf-plugins-core
@@ -23,7 +23,7 @@ wait
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 wait 
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo sh -c 'echo -e "[code]\nname=Visual Stcom.mattjakeman.ExtensionManager udio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 wait
 dnf check-update
 wait
@@ -41,5 +41,12 @@ wait
 
 sudo flatpak install io.dbeaver.DBeaverCommunity com.spotify.Client com.mattjakeman.ExtensionManager 
 wait
+
+install_path = "/home/erald/Documents/Install/"
+config_path = "/home/erald/.config/"
+
+mv "$install_path+kitty.conf"  "$config_path+kitty/kitty.conf"
+mv "$install_path+config.fish"  "$config_path+fish/config.fish"
+mv "$install_path+config.conf"  "$config_path+neofetch/config.conf"
 
 chsh -s $(which fish)
