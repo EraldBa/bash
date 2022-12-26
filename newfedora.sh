3700ce7de90a259d83bb8b0512c780de5d961104
EraldBa <owlyice33@hotmail.com>
2022-12-26 22:40:40 +0200

    Update

--- newfedora.sh
+++ newfedora.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
-
 # For redhat linux systems
+GREEN='\033[0;32m'
+
 function success() {
-    echo "Installed $1 successfully!"
+    printf "\n\n${GREEN}Installed $1 successfully!\n\n"
 }
 
 
 sudo dnf upgrade --refresh
 wait 
-echo "Updated successfully!"
+printf "\n\n${GREEN}Updated successfully!\n\n"
 
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 wait
 success "rpm"
 
-sudo dnf install kitty fish neovim bat vlc bpytop firewalld steam neofetch clocate
+sudo dnf install kitty fish neovim bat vlc bpytop firewalld steam neofetch locate
 wait
 success "first batch"
+# For i3
+sudo dnf install rofi feh
+wait
+success "second batch; i3 utils"
 
 sudo dnf install dnf-plugins-core
 wait
@@ -31,7 +36,7 @@ success "Brave Browser"
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 wait 
-sudo sh -c 'echo -e "[code]\nname=Visual Stcom.mattjakeman.ExtensionManager udio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo sh -c 'printf -e "[code]\nname=Visual Stcom.mattjakeman.ExtensionManager udio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 wait
 dnf check-update
 wait
@@ -45,27 +50,30 @@ success "VS Code"
 # success "Nvidia Drivers"
 # sudo dnf remove xorg-x11-drv-nouveau
 # wait
+# sudo systemd-run --on-active="2h" --unit nvidia-powerd.service                                                                                       0 (0.000s) < 21:43:
+# printf "\n\n${GREEN}Enabled scheduled restart of nvidia-power every 2 hours\n\n"
+
 
 # Here come the flatpaks
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 wait
 sudo flatpak update
 wait
-echo "Updated flatpaks successfully!"
+success "flatpak repo"
 
 sudo flatpak install io.dbeaver.DBeaverCommunity com.spotify.Client com.mattjakeman.ExtensionManager 
 wait
-success "flatpaks"
+success "flatpak packages"
 
-install_path = "/home/erald/Documents/Install/"
-config_path = "/home/erald/.config/"
+# install_path = "/home/erald/Documents/Install/"
+# config_path = "/home/erald/.config/"
 
-mv "${install_path}kitty.conf"  "${config_path}kitty/kitty.conf"
-mv "${install_path}config.fish"  "${config_path}fish/config.fish"
-mv "${install_path}config.conf"  "${config_path}neofetch/config.conf"
+# mv "${install_path}kitty.conf"  "${config_path}kitty/kitty.conf"
+# mv "${install_path}config.fish"  "${config_path}fish/config.fish"
+# mv "${install_path}config.conf"  "${config_path}neofetch/config.conf"
 
-echo "Moved all configs!"
+# printf "\n\n${GREEN}Moved all configs!\n\n"
 
 chsh -s $(which fish)
 
-echo "Changed default shell to fish!"
+printf "\n\n${GREEN}Changed default shell to fish!\n\n"
